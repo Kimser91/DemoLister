f93b7c50fcaaa9410bcf7290948a00063a888301
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2024-12-10 13:38:51 +0100

    Add project files.

---
+++ DemoLister/Item.cs
@@ -0,0 +1,30 @@
+﻿namespace DemoLister;
+
+public class Item
+{
+    private string Name;
+    private string Type;
+    private int Price;
+
+    public Item(string name, string type, int price)
+    {
+        Name = name;
+        Type = type;
+        Price = price;
+    }
+
+    public string getName()
+    {
+        return Name;
+    }
+
+    public int getPrice()
+    {
+        return Price;
+    }
+
+    public void setPrice(int input)
+    {
+        Price = input;
+    }
+}
---
+++ DemoLister/Program.cs
@@ -0,0 +1,33 @@
+﻿using DemoLister;
+
+List<Item> Items = new List<Item>()
+    {
+        new Item("Lightbulb", "Electronics", 10),
+        new Item("Lighter", "Personal", 12),
+        new Item("Apple", "Food", 3),
+        new Item("Diaper", "Baby", 60)
+    };
+PrintList();
+
+void PrintList()
+{
+    foreach (var item in Items)
+    {
+        Console.WriteLine(item.getName());
+    }
+
+    for (int i = 0; i < Items.Count; i++)
+    {
+        Console.WriteLine($"{i}. {Items[i].getName()}");
+    }
+
+    Console.WriteLine("full info");
+    int input = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"{Items[input].getName()} {Items[input].getPrice()}");
+    Console.WriteLine("new Price");
+    int newPrice = int.Parse(Console.ReadLine());
+    Items[input].setPrice(newPrice);
+    Console.WriteLine($"{Items[input].getName()} {Items[input].getPrice()}");
+}
+   
